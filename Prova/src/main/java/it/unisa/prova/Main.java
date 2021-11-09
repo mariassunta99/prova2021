5b135b4011193a99db25835e78dc62dc1aa06203
DELL <DELL@DESKTOP-UBH38HB>
2021-11-09 16:35:56 +0100

    primo commit

---
+++ Prova/src/main/java/it/unisa/prova/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.prova;
+
+/**
+ *
+ * @author DELL
+ */
+public class Main {
+    
+    public static void main(String args[]) {
+        salute("Fisciano");
+        System.out.println("Hello, universe");
+        System.out.println("Hello, metaverse");
+        salute("universe");
+    }
+
+    public static void salute(String whom) {
+        System.out.println("Hello, " + whom);
+    }
+    
+}
